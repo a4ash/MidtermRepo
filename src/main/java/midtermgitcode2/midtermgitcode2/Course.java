0f4a4ebc7ac15e2f176cdc547c1ee7034b722053
a4ash <ashmohnani2001@gmail.com>
2025-10-30 09:13:25 -0400

    rev 5

---
+++ src/main/java/midtermgitcode2/midtermgitcode2/Course.java
@@ -0,0 +1,20 @@
+package midtermgitcode2.midtermgitcode2;
+
+public class Course {
+	private Object _code;
+	private Object _program;
+	private int code;
+	private int program;
+
+	public Course() {
+		throw new UnsupportedOperationException();
+	}
+
+	public void enrolStudent() {
+		throw new UnsupportedOperationException();
+	}
+
+	public void getGrade() {
+		throw new UnsupportedOperationException();
+	}
+}
---
+++ src/main/java/midtermgitcode2/midtermgitcode2/Student.java
@@ -0,0 +1,20 @@
+package midtermgitcode2.midtermgitcode2;
+
+public class Student {
+	private Object _id;
+	private Object _name;
+	private int id;
+	private int name;
+
+	public Student() {
+		throw new UnsupportedOperationException();
+	}
+
+	public void getGpa() {
+		throw new UnsupportedOperationException();
+	}
+
+	public void payTuition() {
+		throw new UnsupportedOperationException();
+	}
+}
